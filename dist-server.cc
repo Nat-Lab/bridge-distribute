8f6a5e0b579fddbf8904829cf49640ee5b166c70
Honghao Zeng <nat@nat.moe>
2019-04-06 03:39:50 -0400

    dist-server: read only needed bytes.

--- dist-server.cc
+++ dist-server.cc
@@ -27,25 +27,21 @@ void DistributionServer::JoinThreads () {
 }
 
 bool DistributionServer::HandleConnect(int fd) {
-    uint8_t *buffer = (uint8_t *) malloc(4096);
-    ssize_t len = read(fd, buffer, 4096);
+    handshake_msg_t hs;
+    ssize_t len = read(fd, &hs, sizeof(handshake_msg_t));
 
     if (len < 0) {
         fprintf(stderr, "[WARN] DistributionServer::HandleConnect: error reading from fd %d: %s.\n", fd, strerror(errno));
-        delete buffer;
         return false;
     }
 
     if (len != sizeof(handshake_msg_t)) {
         fprintf(stderr, "[WARN] DistributionServer::HandleConnect: malformed packet from fd %d.\n", fd);
-        delete buffer;
         return false;
     }
 
-    handshake_msg_t *handshake = (handshake_msg_t *) buffer;
-    fprintf(stderr, "[INFO] DistributionServer::HandleConnect: fd %d joined channel #%d.\n", fd, handshake->id);
-    threads.push_back(dis.AddClient(handshake->id, fd));
-    delete buffer;
+    fprintf(stderr, "[INFO] DistributionServer::HandleConnect: fd %d joined channel #%d.\n", fd, hs.id);
+    threads.push_back(dis.AddClient(hs.id, fd));
     return true;
 }
 
